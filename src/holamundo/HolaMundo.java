15de140b85180ec07d3c1542917553600acb5e7b
Estudiante <Estudiante@BE454S403C02ING.unal.loc>
2019-04-02 08:45:25 -0500

    ejemplo 1

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -1,12 +1,16 @@
 
 package holamundo;
-
+import java.util.*;
 
 public class HolaMundo {
 
    
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        Scanner sc= new Scanner(System.in);
+        System.out.println("#uno");
+        int x=sc.nextInt();
+        System.out.println("ingreso "+x );
     }
     
 }
